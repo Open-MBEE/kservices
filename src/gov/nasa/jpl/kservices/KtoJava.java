5f093c2f55c4bd2df9537dbc06f3258c79e6f95c
Amruta <amruta.yelamanchili@jpl.nasa.gov>
2017-07-26 15:01:44 -0700

    added ability to import things java style

--- src/gov/nasa/jpl/kservices/KtoJava.java
+++ src/gov/nasa/jpl/kservices/KtoJava.java
@@ -812,14 +812,13 @@ public class KtoJava {
         return Arrays.asList( new ClassOrInterfaceType( type.toString() ) );
     }
 
-    //
-    // protected void getImports( Node clsNode ) {
-    // List< String > imports =
-    // XmlUtils.getChildrenElementText( clsNode, "import" );
-    // for ( String imp : imports ) {
-    // addImport( imp );
-    // }
-    // }
+    // right now assumes imports are java imports
+    protected void getImports( ) {
+        List< ImportDecl > imports = new ArrayList<ImportDecl >( JavaConversions.asJavaCollection( model.imports()));
+        for ( ImportDecl imp : imports ) {
+            addImport( imp.toStringNoImport() );
+        }
+    }
     // TODO
 
     public Set< MethodDeclaration > getMethodsForClass( String className ) {
@@ -1260,6 +1259,8 @@ public class KtoJava {
         addImport( "gov.nasa.jpl.ae.event.EventInvocation" );
         addImport( "gov.nasa.jpl.ae.event.DurativeEvent" );
         addImport( "gov.nasa.jpl.ae.event.ParameterListenerImpl" );
+        addImport( "gov.nasa.jpl.ae.event.TimeVarying" );
+        addImport( "gov.nasa.jpl.ae.event.TimeVaryingMap" );
         addImport( "gov.nasa.jpl.ae.event.Event" );
         addImport( "gov.nasa.jpl.ae.solver.ObjectDomain" );
         addImport( "gov.nasa.jpl.mbee.util.Utils" );
@@ -1267,6 +1268,7 @@ public class KtoJava {
         addImport( "gov.nasa.jpl.mbee.util.ClassUtils" );
         addImport( "java.util.Vector" );
         addImport( "java.util.Map" );
+        getImports();
         return getClassData().getCurrentCompilationUnit();
     }
 
