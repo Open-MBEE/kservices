faefbb9d1302e1683e6ef16b3d608f2236ab7e5d
Amruta Yelamanchili <amruta.yelamanchili@jpl.nasa.gov>
2017-11-05 18:41:42 -0800

    started testing framework

---
+++ test/gov/nasa/jpl/kservices/KtoJavaTest.java
@@ -0,0 +1,18 @@
+package gov.nasa.jpl.kservices;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class KtoJavaTest {
+
+    @Test
+    public void testSimple() {
+        String[] simpleK = new String[]{ "src/kTestCases/simple.k"};
+        KtoJava.main(simpleK);
+
+    }
+
+
+
+}
