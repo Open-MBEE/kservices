4ce4ccb95ed7d2ce1ff6d9b1308d4c21f40f1c6c
Bradley J. Clement <bclement@jpl.nasa.gov>
2018-02-12 17:53:12 -0800

    fix typo breaking compile

--- src/gov/nasa/jpl/kservices/k2apgen/Parameter.java
+++ src/gov/nasa/jpl/kservices/k2apgen/Parameter.java
@@ -89,7 +89,7 @@ public class Parameter {
         if ( type.equals("integer") ) return "0";
         if ( type.equals("float") ) return "0.0";
         if ( type.equals("duration") ) return "00:00:01";
-        if ( type.equals("time"[]) ) return "2000-001T00:00:00.000";
+        if ( type.equals("time") ) return "2000-001T00:00:00.000";
         if ( type.equals("array") ) return "[]";
         return "null";  // TODO -- this isn't right, is it?
     }
