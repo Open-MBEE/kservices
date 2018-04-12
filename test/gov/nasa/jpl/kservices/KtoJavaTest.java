b665e705438c2d12b6c4a667d94b9ade4ca1fa5d
Bradley J. Clement <bclement@jpl.nasa.gov>
2018-04-11 18:40:50 -0700

    regression test updates

--- test/gov/nasa/jpl/kservices/KtoJavaTest.java
+++ test/gov/nasa/jpl/kservices/KtoJavaTest.java
@@ -1,8 +1,9 @@
-package test.gov.nasa.jpl.kservices;
+package gov.nasa.jpl.kservices;
 
 import gov.nasa.jpl.ae.xml.EventXmlToJava;
 import gov.nasa.jpl.kservices.KtoJava;
 import gov.nasa.jpl.mbee.util.FileUtils;
+import gov.nasa.jpl.mbee.util.Random;
 import org.junit.Test;
 import java.io.*;
 
@@ -13,6 +14,7 @@ public class KtoJavaTest {
 
     @Test
     public void testSimple() throws FileNotFoundException {
+        Random.reset();
         checkSolution("simple");
     }
 
@@ -31,11 +33,30 @@ public class KtoJavaTest {
         checkSolution("factorial");
     }
 
+    @Test
+    public void testBatteryDemo() throws FileNotFoundException {
+        checkSolution("batteryDemo");
+    }
+
+    // fails
+     @Test
+     public void testAbsFunction() throws FileNotFoundException {
+         checkSolution("absFunction");
+     }
+
     public void checkSolution(String kFileName) throws  FileNotFoundException {
+        Random.reset();
         char fs = File.separatorChar;
         String testCaseDir = "test" + fs + "gov" + fs + "nasa" + fs + "jpl" + fs + "kservices" + fs + "kTestCases" + fs;
+        String otherTestCaseDir = "src" + fs + "kTestCases" + fs;
         String testSolnDir = "test" + fs + "gov" + fs + "nasa" + fs + "jpl" + fs + "kservices" + fs + "kTestCaseSolutions" + fs;
         String inputFile = testCaseDir + kFileName + ".k";
+        File f = new File(inputFile);
+        if ( !f.exists() ) {
+            inputFile = otherTestCaseDir + kFileName + ".k";
+            f = new File(inputFile);
+        }
+        assertTrue( f.exists() );
         String[] input = new String[]{inputFile};
         KtoJava.main(input);
         String outputSolnPath = EventXmlToJava.generatedCodeLocation + fs + "generatedCode" + fs + "solution.log";
